53d900c83edd13f1c1c5bb334dd49ac299ee103b
JH. Chabran <jh@chabran.fr>
2010-01-15 18:00:27 +0100

    corrected an issue with s3 configuration loading

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,1 +1,1 @@
-S3Options = (YAML.load_file("#{RAILS_ROOT}/config/s3.yml")[RAILS_ENV].symbolize_keys rescue Errno::ENOENT).merge(ENV)
+S3Options = (YAML.load_file("#{RAILS_ROOT}/config/s3.yml")[RAILS_ENV].symbolize_keys rescue Errno::ENOENT; {}).merge(ENV)

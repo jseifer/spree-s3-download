d19a3bdfe72cd717f968ce2f60acede06531e2d3
JH. Chabran <jh@chabran.fr>
2010-01-19 14:54:34 +0100

    fixed an issue with missing environment in configuration loading

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
 begin
-  options = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")[RAILS_ENV]
+  options = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")[RAILS_ENV] || {}
 rescue Errno::ENOENT
   options = ENV
 ensure

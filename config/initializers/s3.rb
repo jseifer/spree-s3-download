6c81b452373b901b24576d6c1bcdc24c5d1acaa6
JH. Chabran <jh@chabran.fr>
2010-01-21 16:55:36 +0100

    doh ! ENV is not a hash and thus doesn't respond to #symbolize_keys

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,7 @@
 begin
   options = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")[RAILS_ENV] || {}
 rescue Errno::ENOENT
-  options = ENV
+  options = ENV.to_hash
 ensure
   S3Options = options.symbolize_keys
 end
